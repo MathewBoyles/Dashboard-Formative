b440d43fd42354f89c5fa47690e7b69aa8ce45f1
mathew boyles <845868758@ken20-202-17.local>
2017-08-04 11:51:22 +1200

    4/8/2017

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 google.charts.load("current", {
   packages: ["corechart", "treemap", "geochart"],
-  mapsApiKey: "AIzaSyCAGwOj-7XD8nrxHWl2hwFHj8tvOsy6rJg"
+  mapsApiKey: "AIzaSyBO2Xn3agIAeuTN8e0l4pwupUONZAodSnk"
 });
 
 var dashboard = {
   charts: {},
-  json: {}
+  json: {},
+  colors: {
+    min: "#3366cc",
+    max: "#dc3912",
+    background: "#222",
+    text: "#EEE"
+  }
 };
 
 google.charts.setOnLoadCallback(drawCharts);
@@ -20,24 +26,33 @@ function drawCharts() {
       genderChart();
       ethnicityChart();
       bornChart();
+      travelChart();
+      ratingChart();
+      holidayChart();
     }
   })
 }
 
 function ageChart() {
-  var jsonData = dashboard.json;
   var addData = [
     ["Age", "Number"]
   ];
-  $.each(jsonData.age, function(age, count) {
+  $.each(dashboard.json.age, function(age, count) {
     addData.push([age, count]);
   });
   var data = google.visualization.arrayToDataTable(addData);
 
   var options = {
-    title: "Age",
-    height: 300
+    height: 300,
+    backgroundColor: dashboard.colors.background,
+    legendTextStyle: {
+      color: dashboard.colors.text
+    },
+    titleTextStyle: {
+      color: dashboard.colors.text
+    }
   };
+
   var chart = new google.visualization.PieChart($("#chart-age")[0]);
   chart.draw(data, options);
   dashboard.charts["age"] = {
@@ -48,18 +63,23 @@ function ageChart() {
 }
 
 function genderChart() {
-  var jsonData = dashboard.json;
   var addData = [
     ["Gender", "Number"]
   ];
-  $.each(jsonData.gender, function(gender, count) {
+  $.each(dashboard.json.gender, function(gender, count) {
     addData.push([gender, count]);
   });
   var data = google.visualization.arrayToDataTable(addData);
 
   var options = {
-    title: "Gender",
-    height: 300
+    height: 300,
+    backgroundColor: dashboard.colors.background,
+    legendTextStyle: {
+      color: dashboard.colors.text
+    },
+    titleTextStyle: {
+      color: dashboard.colors.text
+    }
   };
   var chart = new google.visualization.PieChart($("#chart-gender")[0]);
   chart.draw(data, options);
@@ -71,18 +91,23 @@ function genderChart() {
 }
 
 function ethnicityChart() {
-  var jsonData = dashboard.json;
   var addData = [
     ["Ethnicity", "Number"]
   ];
-  $.each(jsonData.ethnicity, function(ethnicity, count) {
+  $.each(dashboard.json.ethnicity, function(ethnicity, count) {
     addData.push([ethnicity, count]);
   });
   var data = google.visualization.arrayToDataTable(addData);
 
   var options = {
-    title: "Ethnicity",
-    height: 300
+    height: 300,
+    backgroundColor: dashboard.colors.background,
+    legendTextStyle: {
+      color: dashboard.colors.text
+    },
+    titleTextStyle: {
+      color: dashboard.colors.text
+    }
   };
   var chart = new google.visualization.PieChart($("#chart-ethnicity")[0]);
   chart.draw(data, options);
@@ -94,7 +119,6 @@ function ethnicityChart() {
 }
 
 function bornChart() {
-  var jsonData = dashboard.json;
   var addData = [
     ["Name", "Parent", "Number"]
   ];
@@ -102,7 +126,7 @@ function bornChart() {
 
   addData.push(["Global", null, 0]);
 
-  $.each(jsonData.born, function(id, data) {
+  $.each(dashboard.json.born, function(id, data) {
     if (hasCountry.indexOf(data.country) < 0) {
       addData.push([data.country, "Global", 0]);
       hasCountry.push(data.country);
@@ -113,9 +137,8 @@ function bornChart() {
   var data = google.visualization.arrayToDataTable(addData);
 
   var options = {
-    minColor: "#f00",
-    midColor: "#ddd",
-    maxColor: "#0d0",
+    minColor: dashboard.colors.min,
+    maxColor: dashboard.colors.max,
     headerHeight: 15,
     fontColor: "black",
     showScale: true,
@@ -140,7 +163,174 @@ function bornChart() {
   };
 }
 
-
 function travelChart() {
+  var addData = [
+    ["Travel Mode", "Number"]
+  ];
+  $.each(dashboard.json.travel, function(mode, count) {
+    addData.push([mode, count]);
+  });
+  var data = google.visualization.arrayToDataTable(addData);
 
+  var options = {
+    height: 500,
+    backgroundColor: dashboard.colors.background,
+    legendTextStyle: {
+      color: dashboard.colors.text
+    },
+    titleTextStyle: {
+      color: dashboard.colors.text
+    },
+    vAxis: {
+      textStyle: {
+        color: dashboard.colors.text
+      }
+    },
+    hAxis: {
+      textStyle: {
+        color: dashboard.colors.text
+      }
+    }
+  };
+  var chart = new google.visualization.BarChart($("#chart-travel")[0]);
+  chart.draw(data, options);
+  dashboard.charts["travel"] = {
+    chart: chart,
+    data: data,
+    options: options
+  };
 }
+
+function ratingChart() {
+  var addData = [
+    ["Rating", "Votes", "Overall Average"]
+  ];
+  var ratings = {
+    "1": 0,
+    "2": 0,
+    "3": 0,
+    "4": 0,
+    "5": 0
+  }
+  var rating_average = 0;
+  $.each(dashboard.json.rating, function(id, rating) {
+    ratings[rating + ""]++;
+    rating_average += rating;
+  });
+
+  rating_average /= dashboard.json.responders;
+
+  addData.push(["1 star", ratings["1"], rating_average]);
+  addData.push(["2 stars", ratings["2"], rating_average]);
+  addData.push(["3 stars", ratings["3"], rating_average]);
+  addData.push(["4 stars", ratings["4"], rating_average]);
+  addData.push(["5 stars", ratings["5"], rating_average]);
+
+
+  var data = google.visualization.arrayToDataTable(addData);
+
+  var options = {
+    height: 500,
+    backgroundColor: dashboard.colors.background,
+    legendTextStyle: {
+      color: dashboard.colors.text
+    },
+    titleTextStyle: {
+      color: dashboard.colors.text
+    },
+    vAxis: {
+      title: "Rating",
+      textStyle: {
+        color: dashboard.colors.text
+      }
+    },
+    hAxis: {
+      title: "Votes",
+      textStyle: {
+        color: dashboard.colors.text
+      }
+    },
+    seriesType: "bars",
+    series: {
+      1: {
+        type: "line"
+      }
+    }
+  };
+  var chart = new google.visualization.ComboChart($("#chart-rating")[0]);
+  chart.draw(data, options);
+  dashboard.charts["rating"] = {
+    chart: chart,
+    data: data,
+    options: options
+  };
+}
+
+function holidayChart() {
+  var addData = [
+    ["City", "Count"]
+  ];
+  $.each(dashboard.json.holiday, function(city, count) {
+    addData.push([city, count]);
+  });
+
+  var data = google.visualization.arrayToDataTable(addData);
+
+  var options = {
+    region: "NZ",
+    displayMode: "markers",
+    colorAxis: {
+      colors: [
+        dashboard.colors.max,
+        dashboard.colors.min
+      ]
+    },
+    height: 700,
+    backgroundColor: dashboard.colors.background,
+    legendTextStyle: {
+      color: dashboard.colors.text
+    },
+    titleTextStyle: {
+      color: dashboard.colors.text
+    }
+  };
+
+  var chart = new google.visualization.GeoChart($("#chart-holiday")[0]);
+  chart.draw(data, options);
+
+  dashboard.charts["holiday"] = {
+    chart: chart,
+    data: data,
+    options: options
+  };
+}
+
+
+$(document).ready(function(){
+  $("#navbar .navbar-brand").click(function(event){
+    $("html,body").animate({
+      scrollTop: 0
+    });
+    event.preventDefault();
+  });
+
+  $("#nav-links a").click(function(event){
+    var do_id = $(this).attr("href");
+    $("html,body").animate({
+      scrollTop: $(do_id).offset()["top"] - 150
+    }, 500, function(){
+      $(do_id).animate({
+        opacity: 0.3
+      }, 100, function(){
+        $(do_id).animate({
+          opacity: 1
+        });
+      });
+    });
+    event.preventDefault();
+  });
+
+  $("#chart-info-box-overlay").click(function(){
+    $("#intermission-wrap").html($("#intermission-wrap").html());
+  })
+})
